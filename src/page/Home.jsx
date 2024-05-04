8f5f7bd5b37e6cd8bff84af29b6555c8f7e1c8b1
Namerana <exalty9741@gmail.com>
2024-05-04 17:24:19 +0200

    add hero to homepage.

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,4 +1,5 @@
-import { Container, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import styles from "./styles/home.module.scss";
 
 export default function Homepage() {
   return (
@@ -6,14 +7,28 @@ export default function Homepage() {
       {/* Hero */}
       <div>
         <h1 className="title">Comment trouver mon artisan ?</h1>
-        <ListGroup className="d-flex flex-column align-items-start">
-          <ListGroupItem>
-            <p>
-              <span className="stepIndex">1.</span> Choisir la catégorie
-              d’artisanat dans le menu.
+        <div className="d-flex flex-column gap-3">
+          <div className="w-100 border rounded-1 p-2">
+            <p className={`${styles["stepDescription"]} m-2`}>
+              1. Choisir la catégorie d’artisanat dans le menu.
             </p>
-          </ListGroupItem>
-        </ListGroup>
+          </div>
+          <div className="w-100 border rounded-1 p-2">
+            <p className={`${styles["stepDescription"]} m-2`}>
+              2. Choisir un artisan.
+            </p>
+          </div>
+          <div className="w-100 border rounded-1 p-2">
+            <p className={`${styles["stepDescription"]} m-2`}>
+              3. Le contacter via le formulaire de contact.
+            </p>
+          </div>
+          <div className="w-100 border rounded-1 p-2">
+            <p className={`${styles["stepDescription"]} m-2`}>
+              4. Une réponse sera apportée sous 48h.
+            </p>
+          </div>
+        </div>
       </div>
     </Container>
   );
