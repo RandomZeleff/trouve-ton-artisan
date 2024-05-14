9d42b14b7af72417c8f6bf705bcf8f9b82601c4d
Namerana <exalty9741@gmail.com>
2024-05-14 18:19:41 +0200

    ajout d'une page d'artisans

--- src/index.js
+++ src/index.js
@@ -9,11 +9,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import general style
 import "./index.scss";
+import { ArtisansPage } from "./page/Artisans";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
+  },
+  {
+    path: "/artisans",
+    element: <ArtisansPage />,
   },
 ]);
 
---
+++ src/page/Artisans.jsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export function ArtisansPage() {
+  return (
+    <Container>
+      <h1 className="title">Liste des artisans</h1>
+    </Container>
+  );
+}
