dfa8d146e27570dfa05eb664a65fe3d93b0d6890
Namerana <exalty9741@gmail.com>
2024-05-28 18:36:18 +0200

    update: utilisation des données json et suppression du container inutile

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,53 +1,44 @@
-import { Container } from "react-bootstrap";
-import styles from "./styles/home.module.scss";
 import artisans from "../data/artisans.json";
 import { ArtisanCard } from "../components/ArtisanCard";
+import Step from "../components/Step";
+
+const steps = [
+  "1. Choisir la catégorie d’artisanat dans le menu.",
+  "2. Choisir un artisan.",
+  "3. Le contacter via le formulaire de contact.",
+  "4. Une réponse sera apportée sous 48h.",
+];
 
 export default function Homepage() {
   return (
-    <Container fluid>
+    <div>
       {/* Hero */}
       <div className="mb-5">
         <h1 className="title">Comment trouver mon artisan ?</h1>
         <div className="d-flex flex-column gap-3">
-          <div className="w-100 border rounded-1 p-2">
-            <p className={`${styles["stepDescription"]} m-2`}>
-              1. Choisir la catégorie d’artisanat dans le menu.
-            </p>
-          </div>
-          <div className="w-100 border rounded-1 p-2">
-            <p className={`${styles["stepDescription"]} m-2`}>
-              2. Choisir un artisan.
-            </p>
-          </div>
-          <div className="w-100 border rounded-1 p-2">
-            <p className={`${styles["stepDescription"]} m-2`}>
-              3. Le contacter via le formulaire de contact.
-            </p>
-          </div>
-          <div className="w-100 border rounded-1 p-2">
-            <p className={`${styles["stepDescription"]} m-2`}>
-              4. Une réponse sera apportée sous 48h.
-            </p>
-          </div>
+          {steps.map((step) => (
+            <Step text={step} />
+          ))}
         </div>
       </div>
 
       {/* Artisans du mois */}
       <div>
         <h2 className="title">Les artisans du mois</h2>
-        <div className="d-flex flex-column gap-3">
+        <div className="d-flex flex-wrap gap-3">
           {artisans.map((artisan) => (
-            <ArtisanCard
-              name={artisan.name}
-              image={artisan.image}
-              localisation={artisan.localisation}
-              speciality={artisan.speciality}
-              note={artisan.note}
-            />
+            <div className="col-12 col-lg-5">
+              <ArtisanCard
+                name={artisan.name}
+                image={artisan.image}
+                localisation={artisan.localisation}
+                speciality={artisan.speciality}
+                note={artisan.note}
+              />
+            </div>
           ))}
         </div>
       </div>
-    </Container>
+    </div>
   );
 }
