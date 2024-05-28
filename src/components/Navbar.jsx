ab718b47aa17559c45a200425d91f1a5edbef9d8
Namerana <exalty9741@gmail.com>
2024-05-28 19:30:07 +0200

    update: ajout du système de recherche des artisans.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import {
   Navbar,
   Button,
   Form,
   Nav,
-  NavDropdown,
+  ListGroup,
   Container,
 } from "react-bootstrap";
+import artisans from "../data/artisans.json";
 
 export default function NavbarComponent() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+
+  const handleSearchChange = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+
+    if (value.length > 2) {
+      // Simuler des résultats de recherche pour la démonstration
+      const results = artisans.filter(
+        (artisan) =>
+          artisan.name.toLowerCase().startsWith(value.toLowerCase()) ||
+          artisan.localisation.toLowerCase().startsWith(value.toLowerCase()) ||
+          artisan.speciality.toLowerCase().startsWith(value.toLowerCase())
+      );
+
+      setSearchResults(results);
+      setShowResults(true);
+    } else {
+      setSearchResults([]);
+      setShowResults(false);
+    }
+  };
+
+  const handleBlur = () => {
+    // Masquer les résultats lorsque l'utilisateur clique hors du menu
+    setShowResults(false);
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
@@ -24,14 +56,27 @@ export default function NavbarComponent() {
             <Nav.Link href="#action2">Fabrication</Nav.Link>
             <Nav.Link href="#action2">Alimentation</Nav.Link>
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex position-relative">
             <Form.Control
               type="search"
               placeholder="Search"
               className="me-2"
               aria-label="Search"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              onBlur={handleBlur}
+              onFocus={() => searchTerm.length > 2 && setShowResults(true)}
             />
-            <Button variant="primary">Search</Button>
+            <Button variant="primary">
+              <i className="fa-solid fa-magnifying-glass" />
+            </Button>
+            {showResults && (
+              <ListGroup className="position-absolute top-100 w-100 mt-2">
+                {searchResults.map((result, index) => (
+                  <ListGroup.Item key={index}>{result.name}</ListGroup.Item>
+                ))}
+              </ListGroup>
+            )}
           </Form>
         </Navbar.Collapse>
       </Container>
