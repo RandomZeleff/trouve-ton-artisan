f7a46546ff2824750033abf1d2e27ea168266ea9
Namerana <exalty9741@gmail.com>
2024-05-28 18:46:08 +0200

    update: Utilisation des composants de react-bootstrap et amélioration de la navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,40 @@
-export default function Navbar() {
+import {
+  Navbar,
+  Button,
+  Form,
+  Nav,
+  NavDropdown,
+  Container,
+} from "react-bootstrap";
+
+export default function NavbarComponent() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
-      <div className="d-flex">
-        <a class="navbar-brand" href="#">
-          Trouve ton Paysan
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Accueil <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Artisans
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <form class="form-inline my-2 my-lg-0 d-flex gap-2">
-        <input
-          class="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button class="btn btn-primary my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
-    </nav>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Trouve ton Artisan</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Bâtiment</Nav.Link>
+            <Nav.Link href="#action2">Services</Nav.Link>
+            <Nav.Link href="#action2">Fabrication</Nav.Link>
+            <Nav.Link href="#action2">Alimentation</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="primary">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
