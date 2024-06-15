1bdbbe06fa2cf9522a8da2206f2da4afebf21775
Namerana <exalty9741@gmail.com>
2024-06-15 13:55:14 +0100

    add: Le footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+export default function Footer() {
+  return (
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-body-secondary">
+            Mentions légales
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-body-secondary">
+            Données personnelles
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-body-secondary">
+            Accessibilité
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-body-secondary">
+            Cookies
+          </a>
+        </li>
+      </ul>
+      <p class="text-center text-body-secondary">
+        101 cours Charlemagne, CS 20033, 69269 LYON CEDEX 02, France, +33 (0)4
+        26 73 40 00
+      </p>
+    </footer>
+  );
+}
