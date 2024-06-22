026380c6907a916bab3fbd3df52df56a16c235b5
Namerana <exalty9741@gmail.com>
2024-06-22 08:10:44 +0100

    feature: envoi d'email via emailjs.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,88 @@
+import emailjs from "emailjs-com";
 import "./styles/contactForm.scss";
+import { useEffect, useState } from "react";
 
 export default function ContactForm() {
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  /**
+   * Permet d'envoyer un mail.
+   * @param {string} to - Destinataire.
+   * @param {string} from - Expéditeur.
+   * @param {string} subject - Objet du mail.
+   * @param {string} message - Message.
+   * @returns {Promise<void>}
+   */
+  const sendMail = async (to, from, subject, message) => {
+    const templateParams = {
+      to_name: to,
+      from_name: from,
+      subject,
+      message,
+    };
+
+    return emailjs.send(
+      process.env.REACT_APP_EMAIL_SERVICE,
+      process.env.REACT_APP_EMAIL_TEMPLATE,
+      templateParams,
+      process.env.REACT_APP_EMAIL_USER
+    );
+  };
+
+  /**
+   * Permet de vérifier que tous les champs du formulaire sont bien remplis.
+   * @returns {string}
+   */
+  const inputNotEmpty = () => {
+    if (!name) return "Veuillez renseigner votre nom.";
+    else if (!subject) return "Veuillez renseigner l'objet du message.";
+    else if (!message) return "Veuillez indiquer un message.";
+    else return "";
+  };
+
+  /**
+   * Permet de supprimer tous les champs du formulaire.
+   */
+  const clearInput = () => {
+    setName("");
+    setSubject("");
+    setMessage("");
+  };
+
+  const handleSubmit = (e) => {
+    // Evite que la page dse recharge.
+    e.preventDefault();
+
+    // Vérifie que les champs ne sont pas vide.
+    const inputError = inputNotEmpty();
+    if (inputError) {
+      setSuccessMessage("");
+      setErrorMessage(inputError);
+      return;
+    }
+
+    try {
+      // Envoi de l'email.
+      sendMail(process.env.REACT_APP_CONTACT_EMAIL, name, subject, message);
+
+      // Supprime les valeurs des champs.
+      clearInput();
+
+      // Affiche un message de succès.
+      setSuccessMessage("Votre message a bien été envoyé !");
+    } catch (err) {
+      console.error("Impossible d'envoyer le mail. ", err);
+      setSuccessMessage("");
+      setErrorMessage("Une erreur est survenue..");
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="form-row mb-3">
         <div className="form-group col-md-6 mb-3 w-100">
           <input
@@ -10,6 +90,8 @@ export default function ContactForm() {
             className="form-control"
             id="nom"
             placeholder="Nom"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="form-group col-md-6 w-100">
@@ -18,15 +100,35 @@ export default function ContactForm() {
             className="form-control"
             id="objet"
             placeholder="Objet"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
           />
         </div>
       </div>
       <div className="form-group">
-        <textarea className="form-control" id="message" placeholder="Message" />
+        <textarea
+          className="form-control"
+          id="message"
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
       </div>
-      <button type="submit" className="btn btn-primary mt-3 px-4 py-2">
-        Envoyer
-      </button>
+      <div className="text-center mb-2">
+        <button type="submit" className="btn btn-primary mt-3 px-4 py-2">
+          Envoyer
+        </button>
+      </div>
+
+      <p
+        className="text-center"
+        style={{
+          display: `${successMessage || errorMessage ? "block" : "none"}`,
+          color: `${successMessage ? "green" : "red"}`,
+        }}
+      >
+        {successMessage || errorMessage || ""}
+      </p>
     </form>
   );
 }
