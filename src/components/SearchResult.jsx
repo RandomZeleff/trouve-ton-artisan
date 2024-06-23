f1c82ef63867b78c448ac8e19dbc921cf1a7dffe
Namerana <exalty9741@gmail.com>
2024-06-23 15:27:58 +0100

    suppression de l'image. Style mis à jour.

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -5,35 +5,18 @@ import { Link } from "react-router-dom";
 export default function SearchResult({ result }) {
   return (
     <ListGroup.Item>
-      <Link to={`/artisans/${result.id}`} className="d-flex gap-3">
-        {/* Image de l'artisan */}
-        <div>
-          <Image
-            src={"/img/fabrice.jpg"}
-            alt={`Photo de ${result.name}`}
-            style={{
-              maxWidth: "50px",
-              height: "50px",
-              objectFit: "cover",
-            }}
-            className="rounded-1"
-          />
-        </div>
-
-        {/* Informations de l'artisan */}
-        <div>
-          <h4 className="h6 title">{result.name}</h4>
-          <p className="text-xs">
-            <span style={{ fontWeight: "bold" }}>Spécialité:</span>{" "}
-            {result.speciality}
-            <br />
-            <span style={{ fontWeight: "bold" }}>Localisation:</span>{" "}
-            {result.location}
-            <br />
-            <span style={{ fontWeight: "bold" }}>Note:</span>{" "}
-            {createStarsNote(Number(Math.round(result.note)))}
-          </p>
-        </div>
+      <Link to={`/artisans/${result.id}`}>
+        <h4 className="h6 title">{result.name}</h4>
+        <p className="text-xs">
+          <span style={{ fontWeight: "bold" }}>Spécialité:</span>{" "}
+          {result.speciality}
+          <br />
+          <span style={{ fontWeight: "bold" }}>Localisation:</span>{" "}
+          {result.location}
+          <br />
+          <span style={{ fontWeight: "bold" }}>Note:</span>{" "}
+          {createStarsNote(Number(Math.round(result.note)))}
+        </p>
       </Link>
     </ListGroup.Item>
   );
